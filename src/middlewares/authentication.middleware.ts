c0e7abc037df544c273abc7506064e2bc6a36845
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-07-11 21:36:35 -0500

    [+] RequestExtends.interface [+] authentication.middleware: Autenticar usuario

--- src/middlewares/authentication.middleware.ts
+++ src/middlewares/authentication.middleware.ts
@@ -1,36 +1,47 @@
 import { Response, Request, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
+import { RequestExtends } from '../interfaces/RequestExtends.interface';
 
 
-const authenticationUser = ( req: Request, res: Response, next: NextFunction ) => {
+const authenticationUser = ( req: RequestExtends, res: Response, next: NextFunction ) => {
     
-    // 1. Procesando la cadena de autorizacion para extraer el token
-    const bearerToken = req.headers.authorization || '';    // bearerToken = Bearer 99999999999
-    const arrBearerToken = bearerToken.split( ' ' );        // arrBearerToken [ 'Bearer', '99999999999' ]
-    const token = arrBearerToken.pop();                     // token - 99999999999, arrBearerToken [ 'Bearer' ]
+    try {
+        
+        // 1. Procesando la cadena de autorizacion para extraer el token
+        const bearerToken = req.headers.authorization || '';    // bearerToken = Bearer 99999999999
+        const arrBearerToken = bearerToken.split( ' ' );        // arrBearerToken [ 'Bearer', '99999999999' ]
+        const token = arrBearerToken.pop();                     // token - 99999999999, arrBearerToken [ 'Bearer' ]
 
-    // 2. Verificar la autenticidad del token
-    const payload = verify( `${ token }`, `${ process.env.JWT_SECRET_KEY }` );
+        // 2. Verificar la autenticidad del token
+        const payload = verify( `${ token }`, `${ process.env.JWT_SECRET_KEY }` );
 
-    // 3. Verificar si NO hay carga util
-    if( ! payload ) {
-        return res.json({
-            msg: 'INVALID_TOKEN'
+        // 3. Verificar si NO hay carga util
+        if( ! payload ) {
+            return res.json({
+                msg: 'INVALID_TOKEN'
+            });
+        }
+
+        // 4. Extraer la carga util
+        // {
+        //     userId:  userFound._id,
+        //     rol: userFound.rol,
+        //     name: userFound.nombre
+        // }
+        const { userId, rol, name } = payload as { userId: string, rol: string, name: string };
+        console.log( userId, rol, name );
+
+        // Agregamos los valores del token al objeto Request de Express usando una nueva Interface (Herencia)
+        req.authUser = { userId, rol, name };       
+
+        next();
+    }
+    catch ( error ) {
+        res.json({
+            msg: 'INVALID_AUTHENTICATION'
         });
     }
 
-    // 4. Extraer la carga util
-    // {
-    //     userId:  userFound._id,
-    //     rol: userFound.rol,
-    //     name: userFound.nombre
-    // }
-    const { userId, rol, name } = payload as { userId: string, rol: string, name: string };
-    console.log( userId, rol, name );
-
-    
-
-    next();
 }
 
 
